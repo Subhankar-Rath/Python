be50eda1be904b4954a269d7ab6d122e95550f10
subhankar rath <subhankarrath07@gmail.com>
2026-03-05 18:10:28 +0530

    assignment

---
+++ python/ass10.py
@@ -0,0 +1,4 @@
+x=45.78
+x1=int(x)
+print("interger part",x1)
+print("fractional part",x-x1)
---
+++ python/ass9.py
@@ -0,0 +1,5 @@
+p=float(input("enter the principal amount: "))
+t=float(input("enter the time period: "))
+r=float(input("enter the rate of interest: "))
+si=p*t*r/100;
+print(si)

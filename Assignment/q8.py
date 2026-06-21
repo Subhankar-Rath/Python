b81caf2600dbf4be6b95f41afe839003d1013634
subhankar rath <subhankarrath07@gmail.com>
2026-06-21 17:41:11 +0530

    assignment

---
+++ Assignment/q8.py
@@ -0,0 +1,18 @@
+m=float(input("enter the number"))
+operator=input("enter the operator(+,-,*,/)")
+n=float(input("enter the number"))
+
+match operator:
+    case "+":
+        print("result:",n+m)
+    case "-":
+        print("result",m-n)
+    case "*":
+        print("result",m*n)
+    case "/":
+        if(n==0):
+            print("invalid result")
+        else:
+            print("result",m/n)
+    case _:
+        print("Invalid result")
